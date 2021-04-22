91b2a86662f0f08f6fb4744d78d11498a8989b9b
Ciro Santilli <ciro.santilli@gmail.com>
2021-04-22 11:53:55 +0100

    Does not clear db on every restart anymore

--- dbConnection.js
+++ dbConnection.js
@@ -37,16 +37,4 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'db.sqlite3',
 });
-
-const checkConnection =async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-checkConnection()
-
 module.exports = sequelize
